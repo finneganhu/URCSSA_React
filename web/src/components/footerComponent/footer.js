cac4208675fe29be5a720ee702f6047827f2b10e
WenqiXiao <44713134+72cacao@users.noreply.github.com>
2019-02-22 17:38:01 -0500

    footer
    minar changes

--- web/src/components/footerComponent/footer.js
+++ web/src/components/footerComponent/footer.js
@@ -7,35 +7,30 @@ import logo from "../../Assets/images/logo/black.jpeg";
 class Footer extends Component {
     render() {
         return (
-
             < footer >
                 <div className="footerContent1">
-
-                    <nav>
+            </div>
+                <nav>
                     <ul>
-                    <li className="first">
-                        <a href="http://www.richmond.edu">University of Richmond</a>
-                        \n 28 Westhampton Way \n Richmond, VA 23173
-                        \n <small>URCSSA 2016-2019</small>
-                    </li>
-                    <li>
-                        Follow Us
-                        //<img id="qrcode" src="devResources/qrCode.jpg" alt="qrcode" height="110"/>
-                    </li>
-                    <li>
-                        Contact Us
-                        //<a href="mailto:cssa@richmond.edu"><img class="emailIcon" src="devResources/emailIcon.png" alt="email" height="25"/> </a>
-                    </li>
-                    <li className="last">
-                        Disclaimer
-                        //<a href="disclaimer.html">View disclaimer</a>
-                    </li>
+                        <li className="first">
+                            <a href="http://www.richmond.edu">University of Richmond</a>
+                            \n 28 Westhampton Way \n Richmond, VA 23173
+                            \n <small>URCSSA 2016-2019</small>
+                        </li>
+                        <li>
+                            Follow Us
+                            <img id="qrcode" src="devResources/qrCode.jpg" alt="qrcode" height="110"/>
+                        </li>
+                        <li>
+                            Contact Us
+                            //<a href="mailto:cssa@richmond.edu"><img class="emailIcon" src="devResources/emailIcon.png" alt="email" height="25"/> </a>
+                        </li>
+                        <li className="last">
+                            Disclaimer
+                            <a href="disclaimer.html">View disclaimer</a>
+                        </li>
                     </ul>
-                    </nav>
-                </div>
-
-
-
+                </nav>
 
             < /footer>
     )
